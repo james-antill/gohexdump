c7b1819a185b04c19a6edeb76fedb40236f27046
James Antill <james@and.org>
2018-04-15 12:29:05 -0400

    Add Dump call.

--- hexdump.go
+++ hexdump.go
@@ -7,22 +7,34 @@ import (
 
 const charactersPerLine = 16
 
-// DumpLine dump 16 bytes of data into a line of hexdump
+// DumpLine dump upto 16 bytes of data into a line of hexdump
 func DumpLine(data []byte, addr int) (string, error) {
-	if len(data) < charactersPerLine {
-		return "", fmt.Errorf("not enough data")
-	}
-
 	ret := fmt.Sprintf("0x%08X:", addr)
 
 	// hexdump data...
-	for i := 0; i < charactersPerLine; i += 2 {
+	llen := charactersPerLine
+	if llen > len(data) {
+		llen = 2 * (len(data) / 2)
+	}
+	i := 0
+	for i = 0; i < llen; i += 2 {
 		ret += fmt.Sprintf(" %02X%02X", data[i+0], data[i+1])
+	}
+	if i < charactersPerLine && i < len(data) {
+		ret += fmt.Sprintf(" %02X  ", data[i+0])
+		i += 2
+	}
+	for ; i < charactersPerLine; i += 2 {
+		ret += "     "
 	}
 	ret += "  "
 
 	// Printable data...
-	for i := 0; i < charactersPerLine; i++ {
+	llen = charactersPerLine
+	if llen > len(data) {
+		llen = len(data)
+	}
+	for i = 0; i < llen; i++ {
 		if false {
 			if unicode.IsPrint(rune(data[i])) {
 				ret += string(data[i])
@@ -45,8 +57,26 @@ func DumpLine(data []byte, addr int) (string, error) {
 			}
 		}
 	}
-
 	ret += "\n"
+
+	return ret, nil
+}
+
+// Dump all data as lines of hexdump
+func Dump(data []byte, addr int) (string, error) {
+	ret := ""
+	for len(data) > 0 {
+		line, err := DumpLine(data, addr)
+		addr += 16
+		ret += line
+		if err != nil {
+			return ret, err
+		}
+		if len(data) < 16 {
+			break
+		}
+		data = data[16:]
+	}
 
 	return ret, nil
 }
--- hexdump_test.go
+++ hexdump_test.go
@@ -2,7 +2,7 @@ package hexdump
 
 import "testing"
 
-func TestLine(t *testing.T) {
+func TestDumpLine(t *testing.T) {
 	data := []struct {
 		off  byte
 		addr int
@@ -45,6 +45,112 @@ func TestLine(t *testing.T) {
 		if val != res {
 			t.Errorf("data not equl: %v %v\n tst=<%s>\n got <%s>\n",
 				off, addr, res, val)
+		}
+	}
+}
+
+func TestDump256(t *testing.T) {
+	data := []struct {
+		off  byte
+		addr int
+		res  string
+	}{
+		{00, 00, "0x00000000: " +
+			"0001 0203 0405 0607 0809 0A0B 0C0D 0E0F  ................\n" +
+			"0x00000010: " +
+			"1011 1213 1415 1617 1819 1A1B 1C1D 1E1F  ................\n" +
+			"0x00000020: " +
+			"2021 2223 2425 2627 2829 2A2B 2C2D 2E2F   !\"#$%&'()*+,-./\n" +
+			"0x00000030: " +
+			"3031 3233 3435 3637 3839 3A3B 3C3D 3E3F  0123456789:;<=>?\n" +
+			"0x00000040: " +
+			"4041 4243 4445 4647 4849 4A4B 4C4D 4E4F  @ABCDEFGHIJKLMNO\n" +
+			"0x00000050: " +
+			"5051 5253 5455 5657 5859 5A5B 5C5D 5E5F  PQRSTUVWXYZ[\\]^_\n" +
+			"0x00000060: " +
+			"6061 6263 6465 6667 6869 6A6B 6C6D 6E6F  `abcdefghijklmno\n" +
+			"0x00000070: " +
+			"7071 7273 7475 7677 7879 7A7B 7C7D 7E7F  pqrstuvwxyz{|}~.\n" +
+			"0x00000080: " +
+			"8081 8283 8485 8687 8889 8A8B 8C8D 8E8F  ................\n" +
+			"0x00000090: " +
+			"9091 9293 9495 9697 9899 9A9B 9C9D 9E9F  ................\n" +
+			"0x000000A0: " +
+			"A0A1 A2A3 A4A5 A6A7 A8A9 AAAB ACAD AEAF  ................\n" +
+			"0x000000B0: " +
+			"B0B1 B2B3 B4B5 B6B7 B8B9 BABB BCBD BEBF  ................\n" +
+			"0x000000C0: " +
+			"C0C1 C2C3 C4C5 C6C7 C8C9 CACB CCCD CECF  ................\n" +
+			"0x000000D0: " +
+			"D0D1 D2D3 D4D5 D6D7 D8D9 DADB DCDD DEDF  ................\n" +
+			"0x000000E0: " +
+			"E0E1 E2E3 E4E5 E6E7 E8E9 EAEB ECED EEEF  ................\n" +
+			"0x000000F0: " +
+			"F0F1 F2F3 F4F5 F6F7 F8F9 FAFB FCFD FEFF  ................\n"},
+	}
+	for i := range data {
+		off := data[i].off
+		addr := data[i].addr
+		res := data[i].res
+
+		data := []byte{}
+		for i := 0; i < 256; i++ {
+			data = append(data, off+byte(i))
+		}
+
+		val, err := Dump(data, addr)
+
+		if err != nil {
+			t.Errorf("err not nil")
+
+		}
+		if val != res {
+			t.Errorf("data not equl: %v %v\n tst=<%s>\n got <%s>\n",
+				off, addr, res, val)
+		}
+	}
+}
+
+func TestDump(t *testing.T) {
+	data := []struct {
+		off  byte
+		addr int
+		num  int
+		res  string
+	}{
+		{00, 00, 20, "0x00000000: " +
+			"0001 0203 0405 0607 0809 0A0B 0C0D 0E0F  ................\n" +
+			"0x00000010: " +
+			"1011 1213                                ....\n"},
+		{0x30, 0, 33, "0x00000000: " +
+			"3031 3233 3435 3637 3839 3A3B 3C3D 3E3F  0123456789:;<=>?\n" +
+			"0x00000010: " +
+			"4041 4243 4445 4647 4849 4A4B 4C4D 4E4F  @ABCDEFGHIJKLMNO\n" +
+			"0x00000020: " +
+			"50                                       P\n"},
+		{0x41, 13, 3, "0x0000000D: " +
+			"4142 43                                  ABC\n"},
+	}
+	for i := range data {
+		off := data[i].off
+		addr := data[i].addr
+		num := data[i].num
+		res := data[i].res
+
+		data := []byte{}
+		for i := 0; i < num; i++ {
+			data = append(data, off+byte(i))
+		}
+
+		val, err := Dump(data, addr)
+
+		if err != nil {
+			t.Errorf("err not nil")
+
+		}
+		if val != res {
+			t.Errorf("data not equl: %v %v %v\n tst=<%s>\n got <%s>\n",
+				off, addr, num, res, val)
 		}
 	}
 }
